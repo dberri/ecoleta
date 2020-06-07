7ec2754caa8a702a9c107598dbcb8beff609f7c0
David Berri <dwbwill@gmail.com>
2020-06-07 11:13:34 -0300

    Handles input data validation

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,6 +4,7 @@ import ItemsController from './controllers/ItemsController'
 
 import multer from 'multer';
 import multerConfig from './config/multer';
+import { celebrate, Joi } from "celebrate";
 
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
@@ -13,8 +14,26 @@ const upload = multer(multerConfig);
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', upload.single('image'), pointsController.create);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required()
+    })
+  }, {
+    abortEarly: false
+  }),
+  pointsController.create
+);
 
 export default routes;
